2c976b4c6f9f063afd3fe03f6d2c150cff0fb240
adam <abanys@gmail.com>
2021-02-04 18:36:32 +0100

    balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,73 +1,127 @@
 //SPDX-License-Identifier: UNLICENSED
 
-// Solidity files have to start with this pragma.
-// It will be used by the Solidity compiler to validate its version.
-pragma solidity ^0.7.0;
+pragma solidity ^0.7.6;
 
-// We import this library to be able to use console.log
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 
-// This is the main building block for smart contracts.
-contract Token {
-    // Some string type variables to identify the token.
-    string public name = "My Hardhat Token";
-    string public symbol = "MHT";
+contract Token is ERC20{
+    using SafeMath for uint256;
 
-    // The fixed amount of tokens stored in an unsigned integer type variable.
-    uint256 public totalSupply = 1000000;
 
-    // An address type variable is used to store ethereum accounts.
-    address public owner;
+    uint public constant supply = 500;
 
-    // A mapping is a key/value map. Here we store each account balance.
-    mapping(address => uint256) balances;
-
-    /**
-     * Contract initialization.
-     *
-     * The `constructor` is executed only once when the contract is created.
-     * The `public` modifier makes a function callable from outside the contract.
-     */
-    constructor() {
-        // The totalSupply is assigned to transaction sender, which is the account
-        // that is deploying the contract.
-        balances[msg.sender] = totalSupply;
-        owner = msg.sender;
+    struct Checkpoint { // TODO determine int size
+        uint256 fromBlock;
+        uint256 value;
     }
 
-    /**
-     * A function to transfer tokens.
-     *
-     * The `external` modifier makes a function *only* callable from outside
-     * the contract.
-     */
-    function transfer(address to, uint256 amount) external {
-        // Check if the transaction sender has enough tokens.
-        // If `require`'s first argument evaluates to `false` then the
-        // transaction will revert.
-        require(balances[msg.sender] >= amount, "Not enough tokens");
+    // currently delegated percentage
+    mapping(address => uint8) delegated;
+    
+    // history of account balance
+    mapping(address => Checkpoint[]) balances;
 
-        // We can print messages and values using console.log
-        console.log(
-            "Transferring from %s to %s %s tokens",
-            msg.sender,
-            to,
-            amount
-        );
 
-        // Transfer the amount.
-        balances[msg.sender] -= amount;
-        balances[to] += amount;
+    constructor() public ERC20("AB", "AB") {
+        // mint some
+        _updateValueAtNow(balances[msg.sender], supply);
+        emit Transfer(address(0), msg.sender, supply);
     }
 
-    /**
-     * Read only function to retrieve the token balance of a given account.
-     *
-     * The `view` modifier indicates that it doesn't modify the contract's
-     * state, which allows us to call it without executing a transaction.
-     */
-    function balanceOf(address account) external view returns (uint256) {
-        return balances[account];
+    function _transfer(address _sender, address _recipient, uint256 _amount) internal override {
+        require(_sender != address(0), "ERC20: transfer from the zero address");
+        require(_recipient != address(0), "ERC20: transfer to the zero address");
+
+        uint256 currentSenderBalance = balanceOfAt(_sender, block.number);
+        require (currentSenderBalance >= _amount, "Sender balance too small");
+
+        uint256 currentRecipientBalance = balanceOfAt(_recipient, block.number);
+        
+        _updateValueAtNow(balances[_sender], currentSenderBalance.sub(_amount));
+        _updateValueAtNow(balances[_recipient], currentRecipientBalance.add(_amount));
+
+        emit Transfer(_sender, _recipient, _amount);
     }
+
+    function balanceOf(address _account) public view override returns (uint256) {
+        return balanceOfAt(_account, block.number);
+    }
+
+    function balanceOfAt(address _address, uint256 _blockNumber) public view returns(uint256) {
+        return _getValueAt(balances[_address], _blockNumber);
+    }
+
+    
+    
+
+    
+
+    /**
+    *** DELEGATE
+     */
+    function delegate(address _delegatee, uint8 _percentage) public {
+        require(_percentage <= 100, "Trying to delegate over 100%");
+        require(_delegatee != msg.sender, "Trying to delegate to self");
+        require(delegated[msg.sender].add(_percentage) <= 100, "Total delegation over 100%");
+        delegated[msg.sender] += _percentage;
+
+        // mapping (address => VoteCheckpoint[])
+        // struct VoteCheckpoint => {fromBlock, list<Pair<Address, percents>}
+
+        // 2nd approach
+        // update 'external' voiting power each time balance changes - problems?
+    }
+
+    function votePowerOfAt(address _address, uint256 _block) public view returns(uint256) {  // TODO - uint256?
+        require(block.number > _block, "Given block in the future");
+        
+        return 0; 
+    } 
+
+    /**
+    ** INTERNAL FUNCTIONS
+    */
+    function _updateValueAtNow(Checkpoint[] storage checkpoints, uint _value) internal  {
+        if ((checkpoints.length == 0) || (checkpoints[checkpoints.length -1].fromBlock < block.number)) {
+            checkpoints.push(Checkpoint(block.number, _value));
+        } else {
+            checkpoints[checkpoints.length -1].value = _value;
+        }
+    }
+
+    function _getValueAt(Checkpoint[] storage checkpoints, uint256 _blockNumber) internal view returns(uint256) {
+        require(_blockNumber <= block.number, "Block is in the future");  // LESS OR LE
+
+        uint nCheckpoints = checkpoints.length;
+        if (nCheckpoints == 0) {
+            return 0;
+        }
+
+        if (checkpoints[nCheckpoints - 1].fromBlock <= _blockNumber) {
+            return checkpoints[nCheckpoints - 1].value;
+        }
+
+        if (checkpoints[0].fromBlock > _blockNumber) {
+            return 0;
+        }
+
+        uint lower = 0;
+        uint upper = nCheckpoints - 1;
+        while (upper > lower) {
+            uint center = upper - (upper - lower) / 2; // ceil, avoiding overflow
+            Checkpoint memory cp = checkpoints[center];
+            if (cp.fromBlock == _blockNumber) {
+                return cp.value;
+            } else if (cp.fromBlock < _blockNumber) {
+                lower = center;
+            } else {
+                upper = center - 1;
+            }
+        }
+        return checkpoints[lower].value;
+    }
+
 }
--- test/Token.js
+++ test/Token.js
@@ -1,134 +1,60 @@
-// This is an exmaple test file. Hardhat will run every *.js file in `test/`,
-// so feel free to add new ones.
-
-// Hardhat tests are normally written with Mocha and Chai.
-
-// We import Chai to use its asserting functions here.
 const { expect } = require("chai");
 
-// `describe` is a Mocha function that allows you to organize your tests. It's
-// not actually needed, but having your tests organized makes debugging them
-// easier. All Mocha functions are available in the global scope.
+let Token
+let abToken
+let owner, signer1, signer2, signers
 
-// `describe` recieves the name of a section of your test suite, and a callback.
-// The callback must define the tests of that section. This callback can't be
-// an async function.
-describe("Token contract", function () {
-  // Mocha has four functions that let you hook into the the test runner's
-  // lifecyle. These are: `before`, `beforeEach`, `after`, `afterEach`.
-
-  // They're very useful to setup the environment for tests, and to clean it
-  // up after they run.
-
-  // A common pattern is to declare some variables, and assign them in the
-  // `before` and `beforeEach` callbacks.
-
-  let Token;
-  let hardhatToken;
-  let owner;
-  let addr1;
-  let addr2;
-  let addrs;
-
-  // `beforeEach` will run before each test, re-deploying the contract every
-  // time. It receives a callback, which can be async.
-  beforeEach(async function () {
-    // Get the ContractFactory and Signers here.
-    Token = await ethers.getContractFactory("Token");
-    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
-
-    // To deploy our contract, we just have to call Token.deploy() and await
-    // for it to be deployed(), which happens onces its transaction has been
-    // mined.
-    hardhatToken = await Token.deploy();
-    await hardhatToken.deployed();
-
-    // We can interact with the contract by calling `hardhatToken.method()`
-    await hardhatToken.deployed();
-  });
-
-  // You can nest describe calls to create subsections.
-  describe("Deployment", function () {
-    // `it` is another Mocha function. This is the one you use to define your
-    // tests. It receives the test name, and a callback function.
-
-    // If the callback function is async, Mocha will `await` it.
-    it("Should set the right owner", async function () {
-      // Expect receives a value, and wraps it in an assertion objet. These
-      // objects have a lot of utility methods to assert values.
-
-      // This test expects the owner variable stored in the contract to be equal
-      // to our Signer's owner.
-      expect(await hardhatToken.owner()).to.equal(owner.address);
-    });
-
-    it("Should assign the total supply of tokens to the owner", async function () {
-      const ownerBalance = await hardhatToken.balanceOf(owner.address);
-      expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
-    });
-  });
-
-  describe("Transactions", function () {
-    it("Should transfer tokens between accounts", async function () {
-      // Transfer 50 tokens from owner to addr1
-      await hardhatToken.transfer(addr1.address, 50);
-      const addr1Balance = await hardhatToken.balanceOf(
-        addr1.address
-      );
-      expect(addr1Balance).to.equal(50);
-
-      // Transfer 50 tokens from addr1 to addr2
-      // We use .connect(signer) to send a transaction from another account
-      await hardhatToken.connect(addr1).transfer(addr2.address, 50);
-      const addr2Balance = await hardhatToken.balanceOf(
-        addr2.address
-      );
-      expect(addr2Balance).to.equal(50);
-    });
-
-    it("Should fail if sender doesn’t have enough tokens", async function () {
-      const initialOwnerBalance = await hardhatToken.balanceOf(
-        owner.address
-      );
-
-      // Try to send 1 token from addr1 (0 tokens) to owner (1000 tokens).
-      // `require` will evaluate false and revert the transaction.
-      await expect(
-        hardhatToken.connect(addr1).transfer(owner.address, 1)
-      ).to.be.revertedWith("Not enough tokens");
-
-      // Owner balance shouldn't have changed.
-      expect(await hardhatToken.balanceOf(owner.address)).to.equal(
-        initialOwnerBalance
-      );
-    });
-
-    it("Should update balances after transfers", async function () {
-      const initialOwnerBalance = await hardhatToken.balanceOf(
-        owner.address
-      );
-
-      // Transfer 100 tokens from owner to addr1.
-      await hardhatToken.transfer(addr1.address, 100);
-
-      // Transfer another 50 tokens from owner to addr2.
-      await hardhatToken.transfer(addr2.address, 50);
-
-      // Check balances.
-      const finalOwnerBalance = await hardhatToken.balanceOf(
-        owner.address
-      );
-      expect(finalOwnerBalance).to.equal(initialOwnerBalance - 150);
-
-      const addr1Balance = await hardhatToken.balanceOf(
-        addr1.address
-      );
-      expect(addr1Balance).to.equal(100);
-
-      const addr2Balance = await hardhatToken.balanceOf(
-        addr2.address
-      );
-      expect(addr2Balance).to.equal(50);
-    });
-  });
+beforeEach(async function () {
+  Token = await ethers.getContractFactory("Token");
+  [owner, signer1, signer2, ...signers] = await ethers.getSigners();
+  abToken = await Token.deploy();
+  await abToken.deployed();
 });
+
+
+describe("Transfers and balances",  ()=> {
+
+  it("Should fail when send to 0x0", async () => {
+    await expect(abToken.transfer(ethers.constants.AddressZero, 123)).to.be.revertedWith("ERC20: transfer to the zero address")
+  })
+
+  it("Should fail when send more than balance", async () => {
+    await expect(abToken.transfer(signer1.getAddress(), 511)).to.be.revertedWith("Sender balance too small")
+  })
+
+  it("Should show correct balance after transfer", async () => {
+    const startBlock = await ethers.provider.getBlockNumber()
+    const transferAmount = 111
+    const ownerBalanceStart = await abToken.balanceOf(owner.getAddress())
+    const signer1BalanceStart = await abToken.balanceOf(signer1.getAddress())
+
+    await abToken.transfer(signer1.getAddress(), transferAmount)
+
+    expect(await abToken.balanceOf(owner.getAddress())).to.be.eq(ownerBalanceStart-transferAmount)
+    expect(await abToken.balanceOf(signer1.getAddress())).to.be.eq(transferAmount)
+    expect(await abToken.balanceOfAt(owner.getAddress(), startBlock)).to.be.eq(ownerBalanceStart)
+    expect(await abToken.balanceOfAt(signer1.getAddress(), startBlock)).to.be.eq(signer1BalanceStart)
+  })
+
+  it("Should fail when asking for future block", async () => {
+    const blockNum = await ethers.provider.getBlockNumber()
+    await expect(abToken.balanceOfAt(owner.getAddress(), blockNum+1)).to.be.revertedWith("Block is in the future")
+  })
+})
+
+
+// describe("Delegation",  ()=> {
+//   it("Should fail when delegate to self", async () => {
+//     await expect(abToken.delegate(owner.getAddress(), 1)).to.be.revertedWith("Trying to delegate to self")
+//   })
+
+//   it("Should fail when delegate over 100 percent", async () => {
+//     await expect(abToken.delegate(signer1.getAddress(), 111)).to.be.revertedWith("Trying to delegate over 100%")
+//   })
+
+//   it("Should fail when total delegation over 100 percent", async () => {
+//     await abToken.delegate(signer1.getAddress(), 90)
+//     await expect(abToken.delegate(signer2.getAddress(), 11)).to.be.revertedWith("Total delegation over 100%")
+//   })
+
+// })
